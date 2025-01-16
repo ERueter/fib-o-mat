326578f1d12a882486c18f02cc7925f76759302b
Victor Deinhart <victor.deinhart@helmholtz-berlin.de>
2025-01-16 13:46:34 +0100

    Bump version: 0.5.0 → 0.5.1

--- src/fibomat/__init__.py
+++ src/fibomat/__init__.py
@@ -9,6 +9,6 @@ from fibomat.units import U_, Q_
 
 import fibomat.default_backends
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 __all__ = ["__version__", "Sample", "Site", "Pattern", "Mill", "Vector", "U_", "Q_"]
