6c8c11d64de45910b1d07dca8220a61c3c2e6bbf
Emma Rüter <emma_rueter@gmx.de>
2025-10-05 09:10:41 +0530

    structure file

--- src/fibomat/optimize/vasile-structured-testfile.py
+++ src/fibomat/optimize/vasile-structured-testfile.py
@@ -6,22 +6,25 @@ from scipy.sparse.linalg import LinearOperator, lsqr
 
 import vasile_with_fft_structured as vas
 
+############################################################################
+# Define target 
+############################################################################
 n = 400
 Z_target = np.zeros((n, n))
 dx = dy = 0.025e-6
 
 # Parameter
-radius_sil = 2.0e-6   # innerer Kugelradius
-radius = 3.0e-6       # äußerer Abfallradius
-height = 5.0e-6       # maximale Höhe in der Mitte
+radius_sil = 2.0e-6   # inner radius
+radius = 3.0e-6       # outer radius
+height = 5.0e-6       # maximal depth
 
-# Koordinatenraster
+# Coordinate grid
 x = (np.arange(n) - n//2) * dx
 y = (np.arange(n) - n//2) * dy
 X, Y = np.meshgrid(x, y, indexing="ij")
 dist = np.sqrt(X**2 + Y**2)
 
-# Profil berechnen
+# Calculate target profile
 Z_target = np.zeros_like(dist)
 
 inside_sil = dist < radius_sil
@@ -30,4 +33,11 @@ Z_target[inside_sil] = height - (height / radius_sil) * np.sqrt(radius_sil**2 - 
 between = (dist >= radius_sil) & (dist < radius)
 Z_target[between] = height * (1 - (dist[between] - radius_sil) / (radius - radius_sil))
 
-vas.compute_grad(Z_target, dx, dy, verbose=True)
+#############################################################################################
+
+
+# Test gradient computation
+dx, dy = vas.compute_grad(Z_target, dx, dy, verbose=True)
+
+# Test sputter yield computation
+S_theta = vas.update_S_from_Z(Z_target, dx, dy, numpy=False, verbose = True)
